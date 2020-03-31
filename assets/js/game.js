9d2dea7b41074dca061b8c96883a7cc1adbc9bb3
Kiran88 <62086331+riazk88@users.noreply.github.com>
2020-03-31 12:03:15 -0600

    robot's name

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10+10);
+console.log("Our robot's name is " + playerName);
+var name = "Kiran";
+console.log(name);
+function fight() {
+    window.alert("The fight has begun!");
+  }
+// fight();
